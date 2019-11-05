b51b720eb485419d459fdfb7340fda997bb7cd08
Temitope Akinsoto <takinsoto@gmail.com>
2019-11-05 16:34:23 +0100

    add POST / endpoint to our routes file
    In this commit, I added a POST / endpoint for creating new car entry to our database.
    This endpoint makes use of the validateCarParams middleware which checks to see if
    the car entry field fulfil the requirements for a valid car entry

--- cars/cars-router.js
+++ cars/cars-router.js
@@ -2,13 +2,56 @@ const express = require('express');
 const db = require('../data/db-config');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    db('cars')
-    .then(cars => {
-      res.status(200).json(cars); 
+function validateCarParams(req, res, next) {
+    const carToBeInserted = req.body;
+    if (!carToBeInserted) {
+      res.status(400).json({ message: "missing car data to be inserted" });
+    } else if (!carToBeInserted.vin) {
+      res.status(400).json({ message: "missing required vin field for car record to be created" });
+    }
+    else if (!carToBeInserted.make) {
+        res.status(400).json({ message: "missing required make field for car record to be created" });
+    } 
+    else if (!carToBeInserted.model) {
+        res.status(400).json({ message: "missing required model field for car record to be created" });
+    }
+    else if (!carToBeInserted.mileage) {
+        res.status(400).json({ message: "missing required mileage field for car record to be created" });
+    }
+    else {
+      next();
+    }
+  }
+
+router.get('/', async(req, res) => {
+    try {
+        db('cars')
+        .then(cars => {
+        res.status(200).json(cars); 
     })
+
+    }catch(error){
+        res.status(500).json({ message: `Failed to retrieve cars records: ${err.message}` });
+    }
+    
+  });
+
+  router.post('/', validateCarParams, (req, res) => {
+    const { vin, make, model, mileage, transmissiontype, status } = req.body;
+    const newCarToBeInserted = {
+        vin,
+        make,
+        model,
+        mileage,
+        transmissiontype,
+        status
+    }
+    db('cars').insert(newCarToBeInserted)
+      .then(newCarEntry => {
+        res.status(201).json(newCarEntry);
+      })
     .catch (err => {
-      res.status(500).json({ message: 'Failed to retrieve cars information' });
+      res.status(500).json({ message: `Failed to store new car entry: ${err.message} `});
     });
   });
   
