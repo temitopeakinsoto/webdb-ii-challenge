c3c4281f5c38001bb6d3b85fc1921badb7672d0a
Temitope Akinsoto <takinsoto@gmail.com>
2019-11-05 15:15:45 +0100

    Create a new car migration file
    In this commit, a migration file for adding a a table to the db was created. Fields added to the table are id, vin, make, model, mileage, integer, transmissiontype and status

--- data/migrations/20191105145331_create-cars-schema.js
+++ data/migrations/20191105145331_create-cars-schema.js
@@ -1,8 +1,27 @@
 
 exports.up = function(knex) {
+    return knex.schema.createTable('cars', table => {
+        table.increments();
+
+        table.text('vin', 128)
+        .unique().notNullAble();
+
+        table.text('make', 128)
+        .notNullAble();
+
+        table.text('model', 128)
+        .notNullAble();
+
+        table.integer('mileage')
+        .notNullAble();
+
+        table.text('transmissiontype');
+
+        table.text('status');
+    })
   
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists('cars');
 };
